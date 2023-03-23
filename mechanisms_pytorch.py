908b4945ec53bbf30e4085fecb6bc054555df835
Chuan Guo <chuanguo@fb.com>
2023-03-23 09:31:31 -0700

    header

--- mechanisms_pytorch.py
+++ mechanisms_pytorch.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
 import torch
 import torch.nn.functional as F
 import numpy as np
